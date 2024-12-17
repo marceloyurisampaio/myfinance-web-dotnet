10b4a62ef4189b71a420833b89a30f09c6988d7a
marceloyurisampaio <marceloyurisampaio@gmail.com>
2024-12-16 21:20:21 -0300

    modified

---
+++ 3 - myfinance-web-dotnet-domain/Entities/Transacao.cs
@@ -0,0 +1,12 @@
+﻿namespace myfinance_web_dotnet_domain.Entities;
+
+public class Transacao
+{
+  public int? Id { get; set; }
+
+  public string Historico { get; set; }
+
+  public DateTime Data { get; set; }
+
+  public decimal Valor { get; set; }
+}
---
+++ 4 - myfinance-web-dotnet-infra/MyFinanceDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace myfinance_web_dotnet_infra;
+
+public class MyFinanceDbContext : DbContext
+{
+  public DbSet<PlanoConta> PlanoConta{get;set;}
+  public DbSet<Transacao> Transacao{get;set;}
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+  {
+    optionsBuilder.UseSqlServer(@"Server=MARCELODELLG3\SQLEXPRESS01;Database=myfinance;Trusted_Connection=True;");
+  }
+}
